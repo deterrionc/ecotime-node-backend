b755f363c4ec47cf5351f3dc5eddea71401149c8
aleksei712 <alekseisolutions@gmail.com>
2022-06-29 20:36:08 +0900

    updated

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const connectDB = require('./config/db');
+// const connectDB = require('./config/db');
 const path = require('path');
 
 const app = express();
 
 // Connect Database
-connectDB();
+// connectDB();
 
 // Init Middleware
 app.use(express.json());
